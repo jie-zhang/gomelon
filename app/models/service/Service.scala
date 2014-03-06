5d1164dc7b992d3426c6b61f3401e705d6ca8f4f
jie-zhang <jie-zhang@rontech.co.jp>
2014-03-06 13:43:44 +0800

    add the initial data for Service and ServiceType.

--- app/models/service/Service.scala
+++ app/models/service/Service.scala
@@ -13,30 +13,32 @@ import play.api.libs.json._
 import play.api.libs.functional.syntax._
 
 case class Service(
-				 id: ObjectId = new ObjectId,
-				 serviceName : String,
-				 serviceType : String,
-				 serviceTsCost : Int,
-				 servicePrice : Int
-                 )
+		id: ObjectId = new ObjectId,
+		serviceName : String,
+		serviceType : String,
+		serviceTsCost : Int,
+		servicePrice : Int
+		)
 
 object Service extends ModelCompanion[Service, ObjectId]{
-  
-  val dao = new SalatDAO[Service, ObjectId](collection = mongoCollection("Service")){}
-  
-  def addService (service :Service) = dao.save(service, WriteConcern.Safe)
-  
-  def getServiceTypeList : List[String] = ServiceType.dao.find(MongoDBObject.empty).toList.map{serviceType =>serviceType.serviceTypeName}
-  
-  def findAllServices : List[Service] = dao.find(MongoDBObject.empty).toList
-  
-  def getTypeList(serviceType:String) : List[Service] = {
-    findAllServices.filter(s =>s.serviceType == serviceType)
-    }
-  
-  def deleteService(id: ObjectId) = dao.remove(MongoDBObject("_id" -> id))
-  
-  def checkService(serviceNm:String): Boolean = dao.find(MongoDBObject("serviceName" -> serviceNm)).hasNext
-  
-  def findOneByServiceId(id: ObjectId): Option[Service] = dao.findOne(MongoDBObject("_id" -> id))
+
+	val dao = new SalatDAO[Service, ObjectId](collection = mongoCollection("Service")){}
+
+	def addService (service :Service) = dao.save(service, WriteConcern.Safe)
+
+	def getServiceTypeList : List[String] = ServiceType.dao.find(MongoDBObject.empty).toList.map {
+		serviceType =>serviceType.serviceTypeName
+	}
+
+	def findAllServices : List[Service] = dao.find(MongoDBObject.empty).toList
+
+	def getTypeList(serviceType:String) : List[Service] = {
+		findAllServices.filter(s =>s.serviceType == serviceType)
+	}
+
+	def deleteService(id: ObjectId) = dao.remove(MongoDBObject("_id" -> id))
+
+	def checkService(serviceNm:String): Boolean = dao.find(MongoDBObject("serviceName" -> serviceNm)).hasNext
+
+	def findOneByServiceId(id: ObjectId): Option[Service] = dao.findOne(MongoDBObject("_id" -> id))
 }
--- app/models/service/ServiceType.scala
+++ app/models/service/ServiceType.scala
@@ -8,12 +8,12 @@ import mongoContext._
 import se.radley.plugin.salat.Binders._
 
 case class ServiceType(
-				 id: ObjectId = new ObjectId,
-				 serviceTypeName : String
-                 )
+		id: ObjectId = new ObjectId,
+		serviceTypeName : String
+		)
 
 object ServiceType extends ModelCompanion[ServiceType, ObjectId]{
-  val dao = new SalatDAO[ServiceType, ObjectId](collection = mongoCollection("ServiceType")){}
-  
-  def addserviceType(serviceType : ServiceType) = dao.save(serviceType, WriteConcern.Safe)
+	val dao = new SalatDAO[ServiceType, ObjectId](collection = mongoCollection("ServiceType")){}
+
+	def addserviceType(serviceType : ServiceType) = dao.save(serviceType, WriteConcern.Safe)
 }
